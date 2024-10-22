74b28678986686b82c355be523d7508789e7941a
Roman Andreeff <di0niixs@gmail.com>
2024-10-22 21:37:18 +0300

    Add task 27

---
+++ 27.rb
@@ -0,0 +1,45 @@
+SIGNS = %w[камень ножницы бумага]
+
+CONDITIONS = [
+  %w[бумага камень user_win],
+  %w[камень ножницы user_win],
+  %w[ножницы бумага user_win],
+  %w[камень бумага computer_win],
+  %w[ножницы камень computer_win],
+  %w[бумага ножницы computer_win]
+]
+
+def handler_for_user_input(user_input)
+  i = 0
+  while i < SIGNS.length
+    if user_input == SIGNS[i][0]
+      return SIGNS[i]
+    end
+    i += 1
+  end
+end
+
+def winner(comp_sign, user_input)
+  return 'Ничья!' if user_input == comp_sign
+  i = 0
+  while i < CONDITIONS.length
+    if CONDITIONS[i][0] == user_input && CONDITIONS[i][1] == comp_sign
+      who_win = CONDITIONS[i][-1]
+      who_win = who_win == 'user_win' ? 'Вы победили!' : 'Победил компьтер!'
+      return who_win
+    end
+    i += 1
+  end
+end
+
+comp_sign = SIGNS.sample
+
+print "Нажмите (К(камень) / Н(ножницы) / Б(бумага) для выбора: "
+
+user_input = gets.strip.downcase
+user_input = handler_for_user_input(user_input)
+
+puts "Компьютер: #{comp_sign}"
+puts "Пользователь: #{user_input}"
+
+puts winner(comp_sign, user_input)
