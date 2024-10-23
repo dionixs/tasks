347ae5359503b271bf5be8bdaa3f67f62cbdb7b8
Roman Andreeff <di0niixs@gmail.com>
2024-10-23 08:12:25 +0300

    Refactoring

--- 15.rb
+++ 15.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
 def leap_year?(year)
-  if year % 4 != 0 || ((year % 100) == 0 && year % 400 != 0)
+  if (year % 4 != 0 || (year % 100) == 0) && year % 400 != 0
     false
-  elsif (year % 100) == 0 && (year % 400) == 0 || (year % 400) == 0
-    true
   else
     true
   end
